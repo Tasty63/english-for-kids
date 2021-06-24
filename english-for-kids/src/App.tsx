2dae6dd56a1044a76ca258b83d33fcb8b1038d48
Tasty <sapegin.petya@yandex.ru>
2021-06-24 22:04:15 +0400

    init: start engish-for-kids

---
+++ english-for-kids/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ReactComponent as Logo } from './logo.svg';
+
+function App() {
+  return (
+    <div className="app">
+      <Logo />
+    </div>
+  );
+}
+
+export default App;
